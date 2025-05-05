ecb6093860df15ff2ace296b4caf7260a4cd0040
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-05-04 18:55:59 -0700

    troubleshooting undeclared issue

--- src/app/prediction/[name]/page.tsx
+++ src/app/prediction/[name]/page.tsx
@@ -1,24 +1,43 @@
+import { get } from "http";
 import React from "react";
+
 //the interface for the object that will be passed as props to the page component
-function getPredictedAge = async(name: string) => {
-const res = await fetch(`https://api.agify.io?name=${name}`)
-return res.json();
-}
-function getPredictedGender= async(name: string) => {
-const res = await fetch(`https://api.genderize.io?name=${name}`)
-return res.json();
-}
-function getPredictedCountry = async(name: string) => {
-const res = await fetch(`https://api.nationalize.io?name=${name}`)
-return res.json();
-}
-interface Params {
-  params: {name: string}
-}
-
-
-const page = ({ params }: Params) => {
-  return <div>{params.name} </div>;
+const getPredictedAge = async (name: string): Promise<any> => {
+  const res = await fetch(`https://api.agify.io?name=${name}`);
+  return res.json();
 };
+const getPredictedGender = async (name: string): Promise<any> => {
+  const res = await fetch(`https://api.genderize.io?name=${name}`);
+  return res.json();
+};
+const getPredictedCountry = async (name: string): Promise<any> => {
+  const res = await fetch(`https://api.nationalize.io?name=${name}`);
+  return res.json();
+};
+interface Params {
+  params: { name: string };
+}
 
-export default page;
+export default async function Page({ params }: Params) {
+  const ageData = getPredictedAge(params.name);
+  const genderData = getPredictedGender(params.name);
+  const countryData = getPredictedCountry(params.name);
+
+  const [age, gender, country] = await Promise.all([
+    ageData,
+    genderData,
+    countryData
+  ]);
+
+  return <div>
+    <div>
+      <div>Personal Info</div>
+      <div>Age</div>
+      <div>{ageData?.age}</div>
+      <div>Gender</div>
+      <div>{genderData?.gender}</div>
+      <div>Country</div>
+      <div>{countryData?.country[0].country_id}</div>
+    </div>
+    </div>;
+}
