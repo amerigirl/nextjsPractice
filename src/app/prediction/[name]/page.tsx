b8340f7722107ac22a7734f804385280b1aa1014
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-05-04 18:33:51 -0700

    creating methods for fetching data

--- src/app/prediction/[name]/page.tsx
+++ src/app/prediction/[name]/page.tsx
@@ -1,4 +1,17 @@
 import React from "react";
+//the interface for the object that will be passed as props to the page component
+function getPredictedAge = async(name: string) => {
+const res = await fetch(`https://api.agify.io?name=${name}`)
+return res.json();
+}
+function getPredictedGender= async(name: string) => {
+const res = await fetch(`https://api.genderize.io?name=${name}`)
+return res.json();
+}
+function getPredictedCountry = async(name: string) => {
+const res = await fetch(`https://api.nationalize.io?name=${name}`)
+return res.json();
+}
 interface Params {
   params: {name: string}
 }
